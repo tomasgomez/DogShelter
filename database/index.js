4e1ec9a92e472f7235c97cac241e1657344b74b1
Filipe Mariano <filipe.mariano.freire@gmail.com>
2018-07-02 22:04:44 -0300

    updated insert test data functionality

--- database/index.js
+++ database/index.js
@@ -1,11 +1,16 @@
 // Require needed modules
 const config = require("./dbconfig.json");
-const auth = config.auth.user + ":" + config.auth.pass;
-const nano = require("nano")("http://" + auth + "@localhost:5984");
 const Promise = require("bluebird");
+const auth = config.auth.user + ":" + config.auth.pass;
+let nano;
+if (auth && auth.user !== "" && auth.pass !== "")
+    nano = require("nano")("http://" + auth + "@localhost:5984");
+else
+    nano = require("nano")("http://localhost:5984");
 
 function createDatabases(insertTestData) {
-    if (insertTestData) docSets = require("./testdata.json").docSets;
+    if (insertTestData)
+        docSets = require("./testdata.json").docSets;
 
     let dbNames = [
         "ds_users",
@@ -25,41 +30,26 @@ function createDatabases(insertTestData) {
         for (dbName of dbNames) {
             (dbName => {
                 if (dbs.indexOf(dbName) === -1) {
-                    let dbCreationReq = createDatabaseAsync(dbName);
                     let req = {};
                     req["dbName"] = dbName;
-                    req["createDb"] = dbCreationReq;
-                    dbCreationReq.then(() => {
-                        if (docSets) {
-                            let DSDocSets = docSets.filter(prop => {
-                                return prop.dbName === dbName;
-                            });
-                            if (DSDocSets.length > 0) {
-                                let createDocAsync = Promise.promisify(
-                                    database.nano.use(dbName).insert
-                                );
-                                req["createDocs"] = [];
-                                for (doc of DSDocSets[0].data) {
-                                    req["createDocs"].push(createDocAsync(doc));
-                                }
-                            }
-                        }
-                    });
+                    req["createDb"] = createDatabaseAsync(dbName);
                     requests.push(req);
                 }
             })(dbName);
         }
 
+        // Check when all databases are created
         let createDbsReqs = requests.map(req => {
             return req.createDb;
         });
-
-        // replaceIDs(requests);
-
         Promise.all(createDbsReqs).then(() => {
-            console.log("all databases were already created");
+            console.log("all databases were created");
             createViews();
         });
+
+        // Insert documents in the databases
+        if (docSets)
+            insertDocs(requests, docSets);
     });
 }
 
@@ -104,49 +94,174 @@ function createViews() {
     );
 }
 
-function replaceIDs(requests) {
-    // In store "ds_pets": replace user IDs
-    let usersReq = requests.filter(req => {
-        return req.dbName === "ds_users";
-    });
-    let petsReq = requests.filter(req => {
-        return req.dbName === "ds_pets";
-    });
-    usersReq[0].createDb.then(() => {
-        Promise.all(usersReq[0].createDocs).then(() => {
-            let users = usersReq[0].createDocs
-            petsReq[0].createDb.then(() => {
-                Promise.all(petsReq[0].createDocs).then(() => {
-                    database.nano.use("ds_pets").list({
-                        include_docs: true
-                    }, (err, result) => {
-                        let pets = result.rows.map(pet => {
-                            return pet.doc;
-                        })
-                        console.log("(replaceIDs) err1 = " + err);
-                        console.log("(replaceIDs) pets = " + JSON.stringify(pets));
-                        console.log("(replaceIDs) users = " + JSON.stringify(users));
-                        if (!err) {
-                            for (pet of pets) {
-                                console.log("pet.userID = " + pet.userID);
-                                console.log("users[pet.userID] = " + users[pet.userID]);
-                                console.log("type = " + typeof (users[pet.userID]));
-                                console.log("fulfillmentValue = " + JSON.stringify(users[pet.userID].fulfillmentValue));
-                                users[pet.userID].then(userInfo => {
-                                    console.log("userInfo = " + JSON.stringify(userInfo));
-                                    pet.userID = userInfo.id;
-                                    database.nano.use("ds_pets").insert(pet, (err, body) => {
-                                        console.log("(replaceIDs) err3 = " + err);
-                                        console.log("(replaceIDs) body = " + body);
-                                    });
-                                });
-                            }
-                        }
-                    });
-                });
-            });
+function insertDocs(requests, docSets) {
+    // Insert docs in 'USERS' database
+    let usersReq = requests.find(req => req.dbName === "ds_users");
+    let usersDocSets = docSets.find(docSet => docSet.dbName === "ds_users");
+    let createUserAsync = Promise.promisify(database.nano.use("ds_users").insert);
+    if (usersDocSets) {
+        usersReq.createDb.then(() => {
+            let createDocsReqs = [];
+            for (user of usersDocSets.data) {
+                createDocsReqs.push(createUserAsync(user));
+            }
+            usersReq["createDocs"] = createDocsReqs;
         });
-    });
+    }
+
+    // Insert docs in 'SERVICES' database
+    let servicesReq = requests.find(req => req.dbName === "ds_services");
+    let servicesDocSets = docSets.find(docSet => docSet.dbName === "ds_services");
+    let createServiceAsync = Promise.promisify(database.nano.use("ds_services").insert);
+    if (servicesDocSets) {
+        servicesReq.createDb.then(() => {
+            let createDocsReqs = [];
+            for (service of servicesDocSets.data) {
+                createDocsReqs.push(createServiceAsync(service));
+            }
+            servicesReq["createDocs"] = createDocsReqs;
+        });
+    }
+
+    // Insert docs in 'PRODUCTS' database
+    let productsReq = requests.find(req => req.dbName === "ds_products");
+    let productsDocSets = docSets.find(docSet => docSet.dbName === "ds_products");
+    let createProductAsync = Promise.promisify(database.nano.use("ds_products").insert);
+    if (productsDocSets) {
+        productsReq.createDb.then(() => {
+            let createDocsReqs = [];
+            for (product of productsDocSets.data) {
+                createDocsReqs.push(createProductAsync(product));
+            }
+            productsReq["createDocs"] = createDocsReqs;
+        });
+    }
+
+    // Insert docs in 'PETS' database
+    let petsReq = requests.find(req => req.dbName === "ds_pets");
+    let petsDocSets = docSets.find(docSet => docSet.dbName === "ds_pets");
+    let createPetAsync = Promise.promisify(database.nano.use("ds_pets").insert);
+    if (petsDocSets) {
+        petsReq.createDb.then(() => {
+            setTimeout(() => {
+                let createDocsReqs = [];
+                for (pet of petsDocSets.data) {
+                    let userID = pet.userID;
+                    let userCreationReq = usersReq.createDocs[userID];
+                    ((pet) => {
+                        userCreationReq.then(user => {
+                            pet.userID = user.id;
+                            createDocsReqs.push(createPetAsync(pet));
+                        });
+                    })(pet);
+                }
+                petsReq["createDocs"] = createDocsReqs;
+            }, 2000);
+        });
+    }
+
+    // Insert docs in 'ORDERS' database
+    let ordersReq = requests.find(req => req.dbName === "ds_orders");
+    let ordersDocSets = docSets.find(docSet => docSet.dbName === "ds_orders");
+    let createOrderAsync = Promise.promisify(database.nano.use("ds_orders").insert);
+    if (ordersDocSets) {
+        ordersReq.createDb.then(() => {
+            setTimeout(() => {
+                let createDocsReqs = [];
+                for (order of ordersDocSets.data) {
+                    let userID = order.userID;
+                    let userCreationReq = usersReq.createDocs[userID];
+                    ((order) => {
+                        userCreationReq.then(user => {
+                            order.userID = user.id;
+                            createDocsReqs.push(createOrderAsync(order));
+                        });
+                    })(order);
+                }
+                ordersReq["createDocs"] = createDocsReqs;
+            }, 2000);
+        });
+    }
+
+    // Insert docs in 'ORDER-PRODUCT-LINES' database
+    let orderPLsReq = requests.find(req => req.dbName === "ds_order_product_lines");
+    let orderPLsDocSets = docSets.find(docSet => docSet.dbName === "ds_order_product_lines");
+    let createOrderPLAsync = Promise.promisify(database.nano.use("ds_order_product_lines").insert);
+    if (orderPLsDocSets) {
+        orderPLsReq.createDb.then(() => {
+            setTimeout(() => {
+                let createDocsReqs = [];
+                for (orderPL of orderPLsDocSets.data) {
+                    let orderProductCreationReqs = [
+                        ordersReq.createDocs[orderPL.orderID],
+                        productsReq.createDocs[orderPL.productID]
+                    ];
+                    ((orderPL) => {
+                        Promise.all(orderProductCreationReqs).then(values => {
+                            orderPL.orderID = values[0].id;
+                            orderPL.productID = values[1].id;
+                            createDocsReqs.push(createOrderPLAsync(orderPL));
+                        });
+                    })(orderPL);
+                }
+                orderPLsReq["createDocs"] = createDocsReqs;
+            }, 5000);
+        });
+    }
+
+    // Insert docs in 'ORDER-SERVICE-LINES' database
+    let orderSLsReq = requests.find(req => req.dbName === "ds_order_service_lines");
+    let orderSLsDocSets = docSets.find(docSet => docSet.dbName === "ds_order_service_lines");
+    let createOrderSLAsync = Promise.promisify(database.nano.use("ds_order_service_lines").insert);
+    if (orderSLsDocSets) {
+        orderSLsReq.createDb.then(() => {
+            setTimeout(() => {
+                let createDocsReqs = [];
+                for (orderSL of orderSLsDocSets.data) {
+                    let orderServiceCreationReqs = [
+                        ordersReq.createDocs[orderSL.orderID],
+                        servicesReq.createDocs[orderSL.serviceID]
+                    ];
+                    ((orderSL) => {
+                        Promise.all(orderServiceCreationReqs).then(values => {
+                            orderSL.orderID = values[0].id;
+                            orderSL.serviceID = values[1].id;
+                            createDocsReqs.push(createOrderSLAsync(orderSL));
+                        });
+                    })(orderSL);
+                }
+                orderSLsReq["createDocs"] = createDocsReqs;
+            }, 5000);
+        });
+    }
+
+    // Insert docs in 'SERVICE-TIME-SLOTS' database
+    let serviceTSsReq = requests.find(req => req.dbName === "ds_service_time_slots");
+    let serviceTSsDocSets = docSets.find(docSet => docSet.dbName === "ds_service_time_slots");
+    let createServiceTSAsync = Promise.promisify(database.nano.use("ds_service_time_slots").insert);
+    if (serviceTSsDocSets) {
+        serviceTSsReq.createDb.then(() => {
+            setTimeout(() => {
+                let createDocsReqs = [];
+                for (serviceTS of serviceTSsDocSets.data) {
+                    let orderServiceLineID = serviceTS.orderServiceLineID;
+                    let SLDocCreationReq = (orderServiceLineID ? orderServiceLineID : null);
+                    let serviceOrderSLCreationReqs = [
+                        servicesReq.createDocs[serviceTS.serviceID],
+                        SLDocCreationReq
+                    ];
+                    ((serviceTS) => {
+                        Promise.all(serviceOrderSLCreationReqs).then(values => {
+                            serviceTS.serviceID = values[0].id;
+                            serviceTS.orderServiceLineID = (values[1] ? values[1].id : null);
+                            createDocsReqs.push(createServiceTSAsync(serviceTS));
+                        });
+                    })(serviceTS);
+                }
+                serviceTSsReq["createDocs"] = createDocsReqs;
+            }, 10000);
+        });
+    }
 }
 
 function removeAllDatabases() {
